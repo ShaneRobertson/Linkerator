186032bb722f4da6b0a7931028762e6db632cc4d
Shane Robertson <sjrobertson0812@gmail.com>
2020-12-24 21:12:53 -0500

    style update

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const { client } = require('./db');
 
 // connect to the server
 const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, async () => {
   console.log(`Server is running on ${ PORT }!`);
 
